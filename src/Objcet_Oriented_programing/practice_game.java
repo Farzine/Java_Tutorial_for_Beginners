2377b356f80072d34fd072702fb2c4afd3ee06e2
Farzine <alfezafarzine@gmail.com>
2023-06-11 00:59:59 +0600

    practice a game

---
+++ src/Objcet_Oriented_programing/practice_game.java
@@ -0,0 +1,15 @@
+package Objcet_Oriented_programing;
+
+public class practice_game {
+    public static void main(String[] args) {
+        /*
+        Create a class game, which allows a user to play "Guess the number"
+        game once. Game should have the following methods:
+        1. Constructor to generate the random number
+        2. takeUserInput() to take a user input of number
+        3. isCorrectNumber() to detect whether the number entered by the user is true
+        4. getter and setter for noOfGuesses
+        use properties such as noOfGuesses(int), etc to get the task done
+         */
+    }
+}
