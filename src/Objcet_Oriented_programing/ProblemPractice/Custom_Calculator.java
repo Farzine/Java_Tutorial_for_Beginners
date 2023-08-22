132ba6b603a6999423dcb22c553c5fa8336849d6
Farzine <alfezafarzine@gmail.com>
2023-08-22 20:06:36 +0600

    ..

---
+++ src/Objcet_Oriented_programing/ProblemPractice/Custom_Calculator.java
@@ -0,0 +1,19 @@
+package Objcet_Oriented_programing.ProblemPractice;
+
+public class Custom_Calculator {
+    public static void main(String[] args) {
+        /*
+        Question: You have to create a custom calculator with following
+        operations:
+        1. + -> addition
+        2. - -> subtraction
+        3. * -> multiplication
+        4. / -> division
+
+        Which throws following exception:
+        1. invalid input, example 8 & 9
+        2. can not divide by 0
+        3. Max input exception, if any of the exception greater than 100000
+         */
+    }
+}
